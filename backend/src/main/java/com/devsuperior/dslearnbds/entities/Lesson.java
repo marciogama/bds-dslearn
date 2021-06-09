e771611c0b3398833b61c2523ae5f1cb7400a568
Marcio Gama <marciogama2000@gmail.com>
2021-06-09 12:40:57 -0300

    Task

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -50,6 +50,9 @@ public abstract class Lesson  implements Serializable{
 	@OneToMany(mappedBy = "lesson")
 	private List<Deliver> deliveries = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "lesson")
+	private List<Topic> topics = new ArrayList<>();
+	
 	public Lesson() {
 	}
 
@@ -99,6 +102,10 @@ public abstract class Lesson  implements Serializable{
 
 	public List<Deliver> getDeliveries() {
 		return deliveries;
+	}
+	
+	public List<Topic> getTopics() {
+		return topics;
 	}
 
 	@Override
